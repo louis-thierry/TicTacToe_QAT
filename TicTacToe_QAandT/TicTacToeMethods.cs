12b34d84cacfad028c1aa55e8f9c7ea57cf7b44c
Louis M Thierry <tue96285@temple.edu>
2018-03-13 18:36:13 -0400

    Lab 6 Submisson

---
+++ TicTacToe_QAandT/TicTacToeMethods.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe_QAandT
+{
+    public class TicTacToeMethods
+    {
+
+        public int[,] gameBoard;
+        public int currentTurn = 1;
+        public char currentPlayer = 'X';
+
+        public void CreateBoard()
+        {
+            this.gameBoard = new int[3, 3];
+
+            for (int i = 0; i < this.gameBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.gameBoard.GetLength(1); j++)
+                {
+                    this.gameBoard[i, j] = 0;
+                    
+                }
+
+            }
+        }
+        public char CurrentPlayer(int currentTurn)
+        {
+            if (currentTurn % 2 == 0)
+            {
+                return this.currentPlayer = 'O';
+            }
+            else
+                return this.currentPlayer = 'X';
+        }
+        public void ChangePlayer()
+        {
+            if (this.currentPlayer == 'X')
+                this. currentPlayer = 'O';
+            else
+                this.currentPlayer = 'X';
+        }
+        public int MakeMove(int positionOnBoardX, int positionOnBoardY, char currentPlayer)
+        {
+            this.currentTurn++;
+
+            if (currentPlayer == 'X')
+            {
+                gameBoard[positionOnBoardX, positionOnBoardY] = 1;
+                CurrentPlayer(currentTurn);
+                return 1;
+            }
+            else
+            {
+                gameBoard[positionOnBoardX, positionOnBoardY] = 2;
+                CurrentPlayer(currentTurn);
+                return 2;
+            }
+        }
+        public Boolean SpaceInUse(int positionOnBoardX, int positionOnBoardY)
+        {
+            if (gameBoard[positionOnBoardX, positionOnBoardY] > 0)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ TicTacToe_QAandTTests/TicTacToeMethodsTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe_QAandT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_QAandT.Tests
+{
+    [TestClass()]
+    public class TicTacToeMethodsTests
+    {
+
+        TicTacToeMethods tictactoeGame = new TicTacToeMethods();
+
+        [TestMethod()]
+        public void CreateBoardTest()
+        {
+            int correctActiveFields = 9;
+            int actualActiveFields;
+
+            tictactoeGame.CreateBoard();
+
+            actualActiveFields = tictactoeGame.gameBoard.Length;
+
+            Assert.AreEqual(correctActiveFields, actualActiveFields);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerTestX()
+        {
+            char isX = 'X';
+
+            if (tictactoeGame.currentPlayer != 'X')
+            {
+                tictactoeGame.ChangePlayer();
+            }
+
+            Assert.AreEqual(isX, tictactoeGame.currentPlayer);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerTestO()
+        {
+            char isO = 'O';
+
+            if (tictactoeGame.currentPlayer != 'O')
+            {
+                tictactoeGame.ChangePlayer();
+            }
+
+            Assert.AreEqual(isO, tictactoeGame.currentPlayer);
+        }
+
+        [TestMethod()]
+        public void placedAnX()
+        {
+            if (tictactoeGame.currentPlayer == 'O')
+                Assert.IsTrue(true);
+            else
+                tictactoeGame.ChangePlayer();
+                Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void placedAnO()
+        {
+            if (tictactoeGame.currentTurn == 'X')
+                Assert.IsTrue(true);
+            else
+                tictactoeGame.ChangePlayer();
+                Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void SpaceInUseTest()
+        {
+            tictactoeGame.CreateBoard();
+
+            int i = 0;
+            int j = 0;
+
+            if(tictactoeGame.SpaceInUse(i,j))
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
